fc510e5556bb207e1b261d812042692b06e93128
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-02-23 04:30:22 -0800

    Create pnz2.cpp

---
+++ pnz2.cpp
@@ -0,0 +1,26 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ int num;
+ cout<<"Enter a number : ";
+ cin>>num;
+ if(num>0)
+{   
+cout<<"number is positive";
+} 
+else if(num==0) 
+{  
+ cout<<"Number is zero";
+} 
+else if(num<0)
+{  
+ cout<<" number is negative" ;
+}
+else
+{
+cout<<"its not a number"'; 
+}
+getch();
+}
