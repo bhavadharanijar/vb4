cb64f829ce5402f8103e619e2136a2e7a21736bb
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-02-25 12:34:54 +0530

    Create al.cpp

---
+++ al.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ int main() 
+{
+    char chr;
+    cout<<"Enter a character\n";
+    cin>>chr;
+  if((chr>='a'&& chr<='z')||(chr>='A'&&c<='Z')) 
+{
+        cout<<chr<<"is an Alphabet.";
+} 
+else
+{
+        cout<<chr<<"is not an Alphabet.";
+}
+   return 0;
+}
