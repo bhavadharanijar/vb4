ee6fd8891873d51ab169ed94a37f851f410bbfac
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-02-25 13:16:56 +0530

    Create pr.cpp

---
+++ pr.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+clrscr();
+int i,num;
+cout<<"enter your number:"<<endl;
+cin>>num;
+ 
+ for(int i=0;i<=num;i++)
+ {
+ cout<<"hello world"<<endl;
+ }
+ }
