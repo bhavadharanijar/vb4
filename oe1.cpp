f62d147d5fdd3cc0c9463572d1d34877bd5532fe
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-02-28 03:55:06 -0800

    Create oe1.cpp

---
+++ oe1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+     char str[20],temp;
+     int i,j;
+     clrscr();
+     cout<<"Enter a string :"<<endl;
+     cin>>str;
+     cout<<"your string is:<<str<<endl;
+     for(i=0;i<strlen(str);i=i+2)
+{
+     temp = str[i];
+     str[i] = str[i+1];
+     str[i+1] = temp;
+}
+     cout<<"Swap String is:<<str<<endl;
+     getch();
+}
