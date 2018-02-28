33943a5b4366c346f7e4986607b70e8903ba605b
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-02-28 04:29:46 -0800

    Update oe1.cpp

--- oe1.cpp
+++ oe1.cpp
@@ -7,13 +7,13 @@ void main()
      clrscr();
      cout<<"Enter a string :"<<endl;
      cin>>str;
-     cout<<"your string is:<<str<<endl;
+     cout<<"your string is:"<<str<<endl;
      for(i=0;i<strlen(str);i=i+2)
 {
      temp = str[i];
      str[i] = str[i+1];
      str[i+1] = temp;
 }
-     cout<<"Swap String is:<<str<<endl;
+     cout<<"Swap String is:"<<str<<endl;
      getch();
 }
