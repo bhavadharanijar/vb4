059508c5f7b124353133cc0c4b70cf9ac0f95c27
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-03-19 17:34:55 +0530

    Create sd2.cpp

---
+++ sd2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+ int a[30];
+ int i, n, a1, j;
+ cin>>n;
+ for (i = 0; i < n; ++i)
+    cin>>a[i]);
+    cin>>a1;
+ for (i = 0; i < a1; ++i)
+ {
+   a[n] = a[0];
+   for (j = 0; j < n; ++j) 
+    number[j] = number[j + 1];
+ }
+
+for (i = 0; i < n; ++i)
+    cout<<a[i];
+}
