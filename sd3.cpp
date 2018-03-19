62d4088911e61053a53b3ba89573834e2182bd7e
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-03-19 17:38:43 +0530

    Create sd3.cpp

---
+++ sd3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+using namespace std;
+int main ()
+{
+ int a2[30],num;
+ int i, n, a1, j;
+ cin>>n;
+ for (i = 0; i < n; ++i)
+    cin>>a2[i]);
+    cin>>a1;
+ for (i = 0; i < a1; ++i)
+ {
+   a2[n] = a2[0];
+   for (j = 0; j < n; ++j) 
+    num[j] = num[j + 1];
+ }
+
+for (i = 0; i < n; ++i)
+    cout<<a2[i];
+}
