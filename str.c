f78a81bc937361243415e2f554b13ffe847a6017
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-05-21 11:31:49 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char r[100],b[100] ;
+	int i ,c=0; 
+	gets(a);
+	gets(b);
+	
+	for(i=0 ; r[i] !='\0' ; i++)
+	{
+		if( r[i] != b[i])
+		{
+			c++;
+		}
+	}
+		if(c == 1)
+		printf("Yes");
+		else
+		printf("No");
+	return();
+}
