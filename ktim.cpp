9d308aa3899bfde621d7f37df26ab85423537065
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-05-25 14:36:00 +0530

    Create ktim.cpp

---
+++ ktim.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   string str;
+   int i,K;
+   cout<<"\nEnter your string";
+   cin>>str;
+   cout<<"\nEnter k value";
+   cin>>K;
+   for(i=0;i<K;i++)
+   {
+   cout<<"\n"<<str<<endl;
+   }
+    return 0;
+}
